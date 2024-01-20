545577e084f331fb33fa3bc242248e96ee9a3bff
AlmostGreenz <38470547+AlmostGreenz@users.noreply.github.com>
2024-01-20 13:28:27 -0500

    Added menu with 'Play' button
    Menu toggleable with ESC and/or can be hidden by pressing the Play button. Time is stopped when menu present.

---
+++ Assets/Scripts/MenuLogic.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLogic : MonoBehaviour
+{
+    private bool paused;
+    public GameObject menu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        paused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Running");
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+            if (paused)
+            {
+                unpauseGame();
+            }
+            else
+            {
+                pauseGame();
+            }
+        }
+    }
+
+    public void pauseGame()
+    {
+        Time.timeScale = 0;
+        menu.SetActive(true);
+    }
+
+    public void unpauseGame()
+    {
+        Time.timeScale = 1;
+        menu.SetActive(false);
+    }
+}
