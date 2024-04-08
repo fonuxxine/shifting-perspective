1729f19fc84ecd8519ac755e5ba8767c369a4e6d
AlmostGreenz <38470547+AlmostGreenz@users.noreply.github.com>
2024-04-08 00:39:51 -0400

    Added dialogue + cutscenes + scene transition to FinalLevel

---
+++ Assets/Scripts/Menus/TimedSceneTransition.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class TimedSceneTransition : MonoBehaviour
+{
+    public float delayTime = 3f;
+    public bool skipCredits = true; // true for Main Menu, false for credits
+    
+    void Start()
+    {
+        if (skipCredits) {
+            Invoke("LoadMainMenu", delayTime);
+        } else {
+            Invoke("LoadCredits", delayTime);
+        }
+    }
+
+    void LoadMainMenu()
+    {
+        SceneLoader.LoadScene(SceneID.MainMenu);
+    }
+
+    void LoadCredits()
+    {
+        SceneLoader.LoadScene(SceneID.ToBeContinued);
+    }
+}
